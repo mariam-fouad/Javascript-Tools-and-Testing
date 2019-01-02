e84d92ee06f4ba904de84028b6e44bb6af309597
Mariam <mariam.fouad94@gmail.com>
2019-01-02 19:02:33 +0300

    first commit

---
+++ Lesson 2/gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task("default", function() {
+  gulp.watch("sass/**/*.scss",["styles"]);
+});
+
+gulp.task("styles", function() {
+    gulp
+    .src("sass/**/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"));
+  });
